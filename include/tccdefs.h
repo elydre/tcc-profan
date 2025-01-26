c20339c7f3d3f255bd01256c5897f44962c17c0b
pf4 <74263128+elydre@users.noreply.github.com>
2025-01-26 13:27:45 +0100

    Update tccdefs.h

--- include/tccdefs.h
+++ include/tccdefs.h
@@ -8,7 +8,10 @@
     in tccpp.c:tcc_predefs().
 */
 
+#define __SIZE_TYPE__ unsigned long
+#define __PTRDIFF_TYPE__ long
 #define __ILP32__ 1
+#define __INT64_TYPE__ long long
 
 #define __SIZEOF_INT__ 4
 #define __INT_MAX__ 0x7fffffff
@@ -21,93 +24,21 @@
 #define __ORDER_BIG_ENDIAN__ 4321
 #define __BYTE_ORDER__ __ORDER_LITTLE_ENDIAN__
 
+#define __WCHAR_TYPE__ int
+#define __WINT_TYPE__ int
+
 #if __STDC_VERSION__ >= 201112L
- #define __STDC_NO_ATOMICS__ 1
- #define __STDC_NO_COMPLEX__ 1
- #define __STDC_NO_THREADS__ 1
- #define __STDC_UTF_16__ 1
- #define __STDC_UTF_32__ 1
+  #define __STDC_NO_ATOMICS__ 1
+  #define __STDC_NO_COMPLEX__ 1
+  #define __STDC_NO_THREADS__ 1
+  #define __STDC_UTF_16__ 1
+  #define __STDC_UTF_32__ 1
 #endif // C11
 
 #define __SIZEOF_SIZE_T__ 4
 #define __SIZEOF_PTRDIFF_T__ 4
 
-//* Some derived integer types needed to get stdint.h to compile correctly on some platforms */
-#define __INT8_TYPE__   signed char
-#define __INT16_TYPE__  short
-#define __INT32_TYPE__  int
-#define __INT64_TYPE__  long long
-#define __UINT8_TYPE__  unsigned char
-#define __UINT16_TYPE__ unsigned short
-#define __UINT32_TYPE__ unsigned int
-#define __UINT64_TYPE__ unsigned long long
-
-#define __INT_LEAST8_TYPE__   __INT8_TYPE__
-#define __INT_LEAST16_TYPE__  __INT16_TYPE__
-#define __INT_LEAST32_TYPE__  __INT32_TYPE__
-#define __INT_LEAST64_TYPE__ __INT64_TYPE__
-#define __UINT_LEAST8_TYPE__  __UINT8_TYPE__
-#define __UINT_LEAST16_TYPE__ __UINT16_TYPE__
-#define __UINT_LEAST32_TYPE__ __UINT32_TYPE__
-#define __UINT_LEAST64_TYPE__ __UINT64_TYPE__
-
-#define __INT_FAST8_TYPE__   __INT8_TYPE__
-#define __INT_FAST16_TYPE__  __INT16_TYPE__
-#define __INT_FAST32_TYPE__  __INT32_TYPE__
-#define __INT_FAST64_TYPE__  __INT64_TYPE__
-#define __UINT_FAST8_TYPE__  __UINT8_TYPE__
-#define __UINT_FAST16_TYPE__ __UINT16_TYPE__
-#define __UINT_FAST32_TYPE__ __UINT32_TYPE__
-#define __UINT_FAST64_TYPE__ __UINT64_TYPE__
-
-#define __INTPTR_TYPE__  __INT32_TYPE__
-#define __UINTPTR_TYPE__ __UINT32_TYPE__
-
-#define __INTMAX_TYPE__  __INT64_TYPE__
-#define __UINTMAX_TYPE__ __UINT64_TYPE__
-
-#define __SIZE_TYPE__    __UINT32_TYPE__
-#define __PTRDIFF_TYPE__ __INT32_TYPE__
-#define __WCHAR_TYPE__   __INT32_TYPE__
-#define __WINT_TYPE__    __INT32_TYPE__
-
-#define __INT8_MAX__        (0x7F)
-#define __INT16_MAX__       (0x7FFF)
-#define __INT32_MAX__       (0x7FFFFFFFL)
-#define __INT64_MAX__       (0x7FFFFFFFFFFFFFFFLL)
-#define __UINT8_MAX__       (0xFF)
-#define __UINT16_MAX__      (0xFFFF)
-#define __UINT32_MAX__      (0xFFFFFFFFL)
-#define __UINT64_MAX__      (0xFFFFFFFFFFFFFFFFLL)
-
-#define __INT_LEAST8_MAX__   __INT8_MAX__
-#define __INT_LEAST16_MAX__  __INT16_MAX__
-#define __INT_LEAST32_MAX__  __INT32_MAX__
-#define __INT_LEAST64_MAX__  __INT64_MAX__
-#define __UINT_LEAST8_MAX__  __UINT8_MAX__
-#define __UINT_LEAST16_MAX__ __UINT16_MAX__
-#define __UINT_LEAST32_MAX__ __UINT32_MAX__
-#define __UINT_LEAST64_MAX__ __UINT64_MAX__
-
-#define __INT_FAST8_MAX__   __INT8_MAX__
-#define __INT_FAST16_MAX__  __INT16_MAX__
-#define __INT_FAST32_MAX__  __INT32_MAX__
-#define __INT_FAST64_MAX__  __INT64_MAX__
-#define __UINT_FAST8_MAX__  __UINT8_MAX__
-#define __UINT_FAST16_MAX__ __UINT16_MAX__
-#define __UINT_FAST32_MAX__ __UINT32_MAX__
-#define __UINT_FAST64_MAX__ __UINT64_MAX__
-
-#define __INTPTR_MAX__  __INT32_MAX__
-#define __UINTPTR_MAX__ __UINT32_MAX__
-
-#define __INTMAX_MAX__  __INT64_MAX__
-#define __UINTMAX_MAX__ __UINT64_MAX__
-
-#define __SIZE_MAX__    __UINT32_MAX__
-#define __PTRDIFF_MAX__ __INT32_MAX__
-#define __WCHAR_MAX__   __INT32_MAX__
-#define __WINT_MAX__    __INT32_MAX__
+#define __INT32_TYPE__ int
 
 /* glibc defines. We do not support __USER_NAME_PREFIX__ */
 #define __REDIRECT(name, proto, alias) name proto __asm__ (#alias)
@@ -147,22 +78,22 @@ typedef char *__builtin_va_list;
 #define __builtin_va_end(ap) (void)(ap)
 
 #ifndef __builtin_va_copy
-# define __builtin_va_copy(dest, src) (dest) = (src)
+  #define __builtin_va_copy(dest, src) (dest) = (src)
 #endif
 
 /* TCC BBUILTIN AND BOUNDS ALIASES */
 #ifdef __leading_underscore
-# define __RENAME(X) __asm__("_"X)
+  #define __RENAME(X) __asm__("_"X)
 #else
-# define __RENAME(X) __asm__(X)
+  #define __RENAME(X) __asm__(X)
 #endif
 
 #ifdef __TCC_BCHECK__
-# define __BUILTINBC(ret,name,params) ret __builtin_##name params __RENAME("__bound_"#name);
-# define __BOUND(ret,name,params) ret name params __RENAME("__bound_"#name);
+  #define __BUILTINBC(ret,name,params) ret __builtin_##name params __RENAME("__bound_"#name);
+  #define __BOUND(ret,name,params) ret name params __RENAME("__bound_"#name);
 #else
-# define __BUILTINBC(ret,name,params) ret __builtin_##name params __RENAME(#name);
-# define __BOUND(ret,name,params)
+  #define __BUILTINBC(ret,name,params) ret __builtin_##name params __RENAME(#name);
+  #define __BOUND(ret,name,params)
 #endif
 
 #define __BOTH(ret,name,params) __BUILTINBC(ret,name,params)__BOUND(ret,name,params)
@@ -214,4 +145,4 @@ __BUILTIN_EXTERN(popcount, unsigned)
 __BUILTIN_EXTERN(parity, unsigned)
 #undef __BUILTIN_EXTERN
 
-#endif /* ndef __TCC_PP__ */
+#endif // __TCC_PP__
